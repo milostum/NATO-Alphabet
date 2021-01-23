301d540f7370d9cbf543a948b707e8a0221bb9b2
Milos Stojanovic <milos.tum@gmail.com>
2021-01-23 19:11:23 +0100

    added try and except

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-input_word = input("Enter a word: ").upper()
-word_list = [phonetic_dict[letter] for letter in input_word]
 
-print(word_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
